6bd62a191f3440d3b25de75113904478eefb7bf8
gersonvidal <gtotrac@gmail.com>
2024-11-13 20:31:24 -0600

    add header model, schema and added it to the global schema

--- hacker_news/schema.py
+++ hacker_news/schema.py
@@ -9,11 +9,12 @@ import skills.schema
 import certificates.schema
 import languages.schema
 import interest.schema
+import header.schema
 
-class Query(interest.schema.Query, languages.schema.Query, certificates.schema.Query, skills.schema.Query, workexperience.schema.Query, education.schema.Query, users.schema.Query, links.schema.Query, graphene.ObjectType):
+class Query(header.schema.Query, interest.schema.Query, languages.schema.Query, certificates.schema.Query, skills.schema.Query, workexperience.schema.Query, education.schema.Query, users.schema.Query, links.schema.Query, graphene.ObjectType):
     pass
 
-class Mutation(interest.schema.Mutation, languages.schema.Mutation, certificates.schema.Mutation, skills.schema.Mutation, workexperience.schema.Mutation, education.schema.Mutation, users.schema.Mutation, links.schema.Mutation, graphene.ObjectType):
+class Mutation(header.schema.Mutation, interest.schema.Mutation, languages.schema.Mutation, certificates.schema.Mutation, skills.schema.Mutation, workexperience.schema.Mutation, education.schema.Mutation, users.schema.Mutation, links.schema.Mutation, graphene.ObjectType):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
